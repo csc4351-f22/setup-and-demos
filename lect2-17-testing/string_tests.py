3a133e8d7d2a965005e32228777ab030707cf9ac
Batya Zamansky <bzamansky@Batyas-MBP.wifi.gsu.edu>
2023-02-15 15:31:14 -0500

    mocking step 1

--- lect2-17-testing/string_tests.py
+++ lect2-17-testing/string_tests.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import patch
 import testing
 
 class TestingStringMethod(unittest.TestCase):
@@ -6,6 +7,10 @@ class TestingStringMethod(unittest.TestCase):
         self.assertEquals("here is a sentence This", testing.stringSwap("This is a sentence here"))
         self.assertFalse(testing.stringSwap("This is a sentence here") == "This is a sentence here")
 
+    @patch('testing.get_joke')
+    def testJoke(self, make_joke):
+        make_joke.return_value = "One"
+        self.assertEquals(testing.len_joke(), 3)
 
 if __name__ == '__main__':
     unittest.main()
--- lect2-17-testing/testing.py
+++ lect2-17-testing/testing.py
@@ -7,3 +7,9 @@ def stringSwap(str):
     sentence = " ".join(words)
     return sentence
 
+def len_joke():
+    joke = get_joke()
+    return len(joke)
+
+def get_joke():
+    return "This is a joke that I am telling"
